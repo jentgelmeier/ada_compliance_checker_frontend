a942631fb6dd5248cb6cf9bb5a65c4935949ca8d
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro-2.local>
2025-09-03 14:30:03 -0400

    add url feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,61 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [htmlInput, SetHtmlInput] = useState("");
+  const [htmlInput, setHtmlInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
   const [issues, setIssues] = useState([]);
   const [error, setError] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [urlToggle, setUrlToggle] = useState(false);
 
-  function handleTextChange(event) {
+  // values that depend on urlToggle
+  const inputText = urlToggle ? "text-body-tertiary" : "";
+  const urlText = urlToggle ? "" : "text-body-tertiary";
+  const placeholder = `Enter html, i.e.,
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Test Page</title>
+  </head>
+  <body>
+    <h1>This is a Test Page</h1>
+  </body>
+</html>`;
+
+  function handleTextChange(event, source) {
     const { value } = event.target;
-    SetHtmlInput(value);
+    if (source === "html") {
+      setHtmlInput(value);
+    } else {
+      setUrlInput(value);
+    }
+  }
+
+  function handleToggle() {
+    setUrlToggle(!urlToggle);
+    setIssues([]);
+    setSubmitted(false);
   }
 
   async function handleSubmit() {
+    let body, requestUrl;
+    if (urlToggle) {
+      body = JSON.stringify({ url: urlInput });
+      requestUrl = "http://127.0.0.1:5000/api/v1/url-check";
+    } else {
+      body = JSON.stringify({ html: htmlInput });
+      requestUrl = "http://127.0.0.1:5000/api/v1/html-check";
+    }
+
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ html: htmlInput }),
+      body,
     };
 
     try {
-      const res = await fetch(
-        "http://127.0.0.1:5000/api/v1/ada-check",
-        requestOptions
-      );
+      const res = await fetch(requestUrl, requestOptions);
 
       if (!res.ok) {
         throw new Error(`HTTP error! status: ${res.status}`);
@@ -53,13 +86,38 @@ function App() {
       <div className="container content mt-5">
         <div className="row">
           <div className="col-md-6 col-s-12 p-2">
-            <span className="fs-5">Input HTML Code</span>
-            <textarea
-              className="overflow-auto"
-              value={htmlInput}
-              onChange={handleTextChange}
-              style={{ height: "600px", width: "100%" }}
-            />
+            <div className="row">
+              <div className="col d-flex">
+                <span className={`fs-5 ${inputText}`}>Input HTML</span>
+                <div class="form-check form-switch d-flex align-items-center ps-5">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    value={urlToggle}
+                    onChange={handleToggle}
+                  />
+                </div>
+                <span className={`fs-5 ${urlText}`}>Input URL</span>
+              </div>
+            </div>
+            {urlToggle ? (
+              <input
+                type="text"
+                className="form-control mb-3"
+                placeholder="Enter a URL, i.e., https://duckduckgo.com"
+                value={urlInput}
+                onChange={(e) => handleTextChange(e, "url")}
+              />
+            ) : (
+              <textarea
+                className="overflow-auto"
+                value={htmlInput}
+                onChange={(e) => handleTextChange(e, "html")}
+                style={{ height: "600px", width: "100%" }}
+                placeholder={placeholder}
+              />
+            )}
             <button
               className="btn btn-primary"
               type="button"
@@ -68,7 +126,7 @@ function App() {
               Submit
             </button>
           </div>
-          <div className="col-md-6 col-s-12 p-2">
+          <div className="col-md-6 col-s-12 p-2" style={{ minHeight: "690px" }}>
             <span className="fs-5">Identified Issues</span>
             <div
               id="issues"
@@ -133,10 +191,18 @@ function IssueBlock({ issues, error, submitted }) {
     return issues.map((issue, index) => (
       <IssueCard issue={issue} index={index} />
     ));
+  } else if (issues.message) {
+    return (
+      <div className="alert alert-danger">
+        {issues.message}
+      </div>
+    );
   } else if (submitted) {
-    return <div className="alert alert-success">
-      Looks good! There were no accessibility issues identified.
-    </div>;
+    return (
+      <div className="alert alert-success">
+        Looks good! There were no accessibility issues identified.
+      </div>
+    );
   } else {
     return "";
   }
